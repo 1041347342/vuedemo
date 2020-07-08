c0a9a9b48ade5d36a8de599b16a9f3198a805b03
z2398381608 <z2398381608@qq.com>
2020-07-08 10:19:43 +0800

    添加navbar组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-    <h2>首页</h2>
+    <div id="home">
+        <nav-bar class="home-nav-bar">
+            <div slot="left">&lt;</div>
+            <div slot="center"><input type="search" placeholder="衣服"></div>
+            <div slot="right">登录</div>
+        </nav-bar>
+    </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue"
 export default {
-    
+    name:"Home",
+    components:{
+        NavBar
+    }
 }
 </script>
 
 <style scoped>
-
+    #home{
+        /* padding-top:40px; */
+        height:100vh;
+    }
+    #home .home-nav-bar{
+        background:#e43130;
+        color:#fff;
+        display: fixed;
+        top:0;
+        left:0;
+        right:0;
+        z-index:100;
+    }
 </style>
